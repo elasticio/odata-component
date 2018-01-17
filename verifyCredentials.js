11734ea03708d9b507d321f9b709b4a73588b6ae
Jacob Horbulyk <jhorbulyk@users.noreply.github.com>
2018-01-17 14:21:15 +0100

    Starting point for a generic OData with OAuth component (#1)
    * Initial write.

---
+++ verifyCredentials.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const ODataClient = require('./lib/oDataClient');
+
+module.exports = async function (cfg) {
+  try {
+    const instance = new ODataClient(this, cfg);
+    // Fetch service document
+    await instance.makeRequest('', 'GET');
+    console.log('Successfully verified credentials.');
+    return true;
+  } catch (e) {
+    // Workaround for https://github.com/elasticio/sailor-nodejs/issues/58
+    console.log(`Exception: ${e.toString()} \n ${e.stack}`);
+    return false;
+  }
+};
