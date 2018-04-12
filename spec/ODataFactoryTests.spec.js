a8d17ff1ea42b6493ce01420e6aeb3efe300212c
Jacob Horbulyk <jacob@elastic.io>
2018-04-12 15:38:04 +0200

    Remove do nothing test.

--- spec/ODataFactoryTests.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-it('Factory Tests', function () {
-  // Verify that all files can be imported
-  require('../lib/actions/upsertObject');
-  require('../lib/actions/lookupObjectByFields');
-  require('../lib/triggers/getObjectsPolling');
-  require('../verifyCredentials');
-});
