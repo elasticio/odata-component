1d99c6278419eb1d37aefcde1a40914089eccc38
ankit singh <asingh@magicsoftware.com>
2018-05-29 21:05:33 +0530

    commit message

---
+++ module.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const statelessAuthenticationRestClients = require('./lib/commons/authentication/statelessAuthenticationRestClients');
+
+module.exports = {
+  Authentication: {
+    OAuthAuthorizationCodeRestClient: require('./lib/commons/authentication/OAuthAuthorizationCodeRestClient'),
+    NoAuthRestClient: statelessAuthenticationRestClients.NoAuthRestClient,
+    BasicAuthRestClient: statelessAuthenticationRestClients.BasicAuthRestClient,
+    ApiKeyRestClient: statelessAuthenticationRestClients.ApiKeyRestClient
+  },
+  OData: {
+    ODataClient: require('./lib/commons/odata/ODataClient')
+  }
+};
